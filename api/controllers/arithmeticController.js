ffb9002be406f398718d7374d43b7a6125df79b0
psanthosh1987 <psanthosh1987@gmail.com>
2022-06-29 05:54:19 -0700

    psanthosh1987.calculator
    additional-cleanup

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -9,7 +9,7 @@ exports.calculate = function(req, res) {
     res.status(400);
     res.json({ error: err.message });
   });
-
+// updated arithmetic operations
   var operations = {
     'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
